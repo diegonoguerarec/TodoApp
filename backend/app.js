9ae1da7036ddf5b5d8e87913f4a27da86a1684e1
Diegao <diegonoguerarec@gmail.com>
2026-04-18 09:45:41 -0300

    Added prisma and very basic post structure - Added prisma as a dependency - Setup of prisma with the container DB - Generated schema for Todo model in schema.prisma - Very basic structure for POST tasks (Just for testing purposes, no validations included yet)

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,38 @@
 const express = require('express');
+require('dotenv').config();
+
+// Prisma setup
+const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { Pool } = require('pg');
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
+
 const app = express();
 const port = 3000;
 
+// Use Json parser middleware
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello world!');
+});
+
+app.post('/todos', async (req, res) => {
+
+    const data = {
+        name: req.body.name,
+        description: req.body.description,
+    };
+
+    const Todo = await prisma.todo.create({
+        data: data
+    });
+
+    res.send({message:"Generated test task"});
 });
 
 app.get('/todos', (req, res) => {
